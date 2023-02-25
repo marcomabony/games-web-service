4960ff43b0076686ac5be184899bec99bfef64f6
marcomabony <marcomabony@hotmail.com>
2023-02-25 15:22:03 -0300

    Inicia games-web-service

---
+++ src/main/java/com/application/gameswebservice/GamesWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.gameswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/gameswebservice/GamesWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.gameswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
